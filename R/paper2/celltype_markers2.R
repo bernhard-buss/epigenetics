48e7c9ca9fb3a1a06fd8b529b2aff4de20c277e7
Tanja Güller <tanja.gueller@uzh.ch>
2024-04-08 11:37:27 +0200

    Pattern matching between list of paper2 & list Rodrigo

---
+++ R/paper2/celltype_markers2.R
@@ -0,0 +1,41 @@
+library(readr)
+
+# positive marker genes used for classification of cell types
+celltype_marker_lists <- list(
+  ProjectionNeurons = c('Neurod2', 'Neurod6', 'Tubb3', 'Tbr1', 'Cux2', 'Cux1', 'Satb2', 'Bhlhe22', 'Hspb3', 'Lmo4', 'Rorb',
+                        'Foxp1', 'Fezf2', 'Pcp4', 'Ldb2', 'Etv1', 'Bcl11b', 'Tle4', 'Syt6', 'Foxp2', 'Zfpm2', 'Crym'), 
+  # unterteile?
+  InhibitoryNeurons = c('Gad1', 'Gad2', 'Slc32a1', 'Erbb4', 'Sst', 'Calb2', 'Vip'),
+  OligodendrocytePrecursorCells = c('Fabp7', 'Ccnd1', 'Cspg4', 'Mdk', 'Ednrb', 'Pdgfra'),
+  CommittedOligodendrocytePrecursors = c('Brca1', 'Pak4', 'Mycl', 'Pdcd4', 'Fyn', 'Bmp4', 'Epcam', ),
+  NewlyFormedOligodendrocytes = c('Sema4d', 'Mob3b', 'Ddc', 'Cnksr3', 'Prom1', 'Fam107b', 'Tmem2', 'Rras2', 'Plekha1',
+                                  'Kndc1', 'Slc9a3r2', 'Tmem141', 'Man1a', 'Prr5l', 'Aspa', 'Anln', 'Ndrg1'),
+  Astrocytes = c('Aldh1l1', 'Slc1a3', 'Apoe', 'Gfap', 'Aqp4'),
+  Microglia = c('Tmem119', 'Aif1'),
+  Pericytes = c('Pdgfrb')
+)
+
+# genes list by Rodrigo
+genes_chromatin <- read.csv("data/paper2/genes_chromatin_mm39.csv")
+
+# Delete 2nd column & change header
+print(names(genes_chromatin))
+genes_chromatin <- genes_chromatin[, !names(genes_chromatin) %in% c('X')]
+new_column_names <- c('gene', 'ID', 'whole_gene_name', 'function')
+names(genes_chromatin) <- new_column_names
+# check
+# print(names(genes_chromatin))
+# View(genes_chromatin)
+# head(genes_chromatin)
+
+# genes that match between the 2 lists:
+matches <- lapply(celltype_marker_lists, function(celltype_markers) {
+  grepl(celltype_markers, genes_chromatin$gene)
+})
+for (i in seq_along(matches)) {
+  cat('Cell Types:', celltype_marker_lists[i], '\n')
+  cat('Matches:', sum(matches[[i]]), '\n')
+}
+
+
+

e0d3a04742c97836a61db14bedbe3e324aab7f95
Bernhard Buss <bernhard.buss@gmail.com>
2024-04-10 14:00:11 +0200

    Generate new heatmaps

--- R/shared/filenames.R
+++ R/shared/filenames.R
@@ -1,9 +1,13 @@
 # Returns the filename to use for a figure and given project and plot name.
 figure_filename <- function(project, topic, name) {
-  return(paste0('output/figures/', project, '/', topic, '/', name, '.png'))
+  filename = paste0('output/figures/', project, '/', topic, '/', name, '.png')
+  dir.create(dirname(filename), recursive = TRUE, showWarnings = FALSE)
+  return(filename)
 }
 
 # Returns the filename to use for a table (CSV) and given project, topic and name.
 table_filename <- function(project, topic, name) {
-  return(paste0('output/tables/', project, '/', topic, '/', name, '.csv'))
+  filename = paste0('output/tables/', project, '/', topic, '/', name, '.csv')
+  dir.create(dirname(filename), recursive = TRUE, showWarnings = FALSE)
+  return(filename)
 }
--- R/shared/plot_DGE_heatmap.R
+++ R/shared/plot_DGE_heatmap.R
@@ -1,6 +1,7 @@
 
 perform_DGE_Two_Days <- function(sobj, celltype, genes, day1, day2) {
-  topic <- paste0(day1, '_', day2)
+  topic = 'DGE'
+  days = paste0(day1, '_', day2)
   
   print(paste0('Performing DGE for ', celltype, ', ', day1, ' vs ', day2))
   
@@ -17,13 +18,11 @@ perform_DGE_Two_Days <- function(sobj, celltype, genes, day1, day2) {
     return()
   }
   # Count the number of cells in each cluster
-  cluster_counts <- table(sobj_celltype@meta.data$seurat_clusters)
-  # Find clusters with fewer than 3 cells
-  clusters_with_few_cells <- intersect(cluster_counts[cluster_counts < 3], c(day1, day2))
+  cluster_counts <- table(sobj_celltype@meta.data$Day)
   # Check if there are any clusters with fewer than 3 cells
-  if (length(clusters_with_few_cells) > 0) {
-    cat("The following days have fewer than 3 cells:\n")
-    print(clusters_with_few_cells)
+  if (cluster_counts[day1] < 3 || cluster_counts[day2] < 3) {
+    cat("At least one day has fewer than 3 cells:\n")
+    print(cluster_counts)
     return()
   }
   
@@ -49,17 +48,18 @@ perform_DGE_Two_Days <- function(sobj, celltype, genes, day1, day2) {
     #pCutoff = 0.05
   ) + ggtitle(paste0(celltype, ' DGE ', day1, ' vs ', day2)) +
     theme(plot.title = element_text(hjust = 0.5))  # This centers the title
-  ggsave(figure_filename(sobj@project.name, topic, paste0(celltype, '_DGE_', topic, '_volcano')), plot = volcano_plot, width = 10, height = 8, dpi = 300)
+  ggsave(figure_filename(sobj@project.name, topic, paste0(celltype, '_DGE_', days, '_volcano')), plot = volcano_plot, width = 10, height = 8, dpi = 300)
   
   print('Generating significant marker list table')
   filtered_chromatin_markers_per_day_significant = filtered_chromatin_markers_per_day[filtered_chromatin_markers_per_day$p_val < 0.05,]
-  write.csv(filtered_chromatin_markers_per_day_significant, file = table_filename(sobj1@project.name, topic, paste0(celltype, '_', topic, '_DGE_markers')))
+  write.csv(filtered_chromatin_markers_per_day_significant, file = table_filename(sobj1@project.name, topic, paste0(celltype, '_', days, '_DGE_markers')))
   
   print('Generating heatmap')
-  DGE_per_day_heatmap = DoHeatmap(sobj_celltype, features = filtered_chromatin_markers_per_day_significant$gene, group.by = 'Day', size = 3, angle = 90) +
-    ggtitle(paste0(celltype, ' DGE ', day1, ' vs ', day2)) +
-    theme(plot.title = element_text(hjust = 0.5))  # This centers the title
-  ggsave(figure_filename(sobj@project.name, topic, paste0(celltype, '_DGE_', topic, '_heatmap')), plot = DGE_per_day_heatmap, width = 10, height = 8, dpi = 300)
+  DGE_per_day_heatmap = dittoHeatmap(sobj_celltype, genes=rownames(filtered_chromatin_markers_per_day_significant), annot.by = 'Day', main = paste0(celltype, ' DGE ', day1, ' vs ', day2))
+  #DGE_per_day_heatmap = DoHeatmap(sobj_celltype, features = rownums(filtered_chromatin_markers_per_day_significant), group.by = 'Day', size = 3, angle = 90) +
+  #  ggtitle(paste0(celltype, ' DGE ', day1, ' vs ', day2)) +
+  #  theme(plot.title = element_text(hjust = 0.5))  # This centers the title
+  ggsave(figure_filename(sobj@project.name, topic, paste0(celltype, '_DGE_', days, '_heatmap')), plot = DGE_per_day_heatmap, width = 10, height = 8, dpi = 300)
 }
 
 
@@ -67,12 +67,6 @@ plot_DGE_heatmap <- function(sobj, celltype, relevant_chromatin_genes) {
   #sobj1_celltype <- subset(sobj1, subset = Cell_Type_heuristic == 'astrocytes')
   sobj_celltype <- subset(sobj, subset = Cell_Type == celltype)
   Idents(sobj_celltype) <- 'Day'
-  
-  volcano_plot <- VariableFeaturePlot(sobj) +
-    ggtitle(paste0(celltype, ' Variable features')) +
-    theme(plot.title = element_text(hjust = 0.5))  # This centers the title
-  ggsave(figure_filename(sobj@project.name, 'DGE', paste0(celltype, '_DGE_per_day_heatmap')), plot = DGE_per_day_heatmap, width = 10, height = 8, dpi = 300)
-
   
   filtered_chromatin_markers_all = FindAllMarkers(
     object = sobj_celltype,

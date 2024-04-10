834136b4494c3f4cab2bf1f5c6de22274d30330b
Bernhard Buss <bernhard.buss@gmail.com>
2024-04-10 15:10:19 +0200

    Add DGE celltype comparison per day

--- R/paper1/paper1.R
+++ R/paper1/paper1.R
@@ -132,84 +132,31 @@ DoHeatmap(sobj1, features = celltype_marker_lists['cpnLayer56']) + NoLegend()
 relevant_chromatin_genes = intersect(as.vector(genes_chromatin$gene), Features(sobj1))
 for (celltype in unique(sobj1@meta.data$Cell_Type)) {
   tryCatch({
-    perform_DGE_Two_Days(sobj1, celltype, genes=relevant_chromatin_genes, day1='E18', day2='P1')
-    perform_DGE_Two_Days(sobj1, celltype, genes=relevant_chromatin_genes, day1='P1', day2='P4')
-    perform_DGE_Two_Days(sobj1, celltype, genes=relevant_chromatin_genes, day1='E18', day2='P4')
+    perform_DGE_two_days(sobj1, genes=relevant_chromatin_genes, celltype=celltype, day1='E18', day2='P1')
+    perform_DGE_two_days(sobj1, genes=relevant_chromatin_genes, celltype=celltype, day1='P1', day2='P4')
+    perform_DGE_two_days(sobj1, genes=relevant_chromatin_genes, celltype=celltype, day1='E18', day2='P4')
   }, error = function(e) {
     # Code to run in case of an error
     cat("An error occurred:", e$message, "\n")
   })
 }
-# Manual single celltype
-# Count the number of cells in each cluster
-Idents(sobj1_celltype) <- 'Day'
-cluster_counts <- table(sobj1_celltype@meta.data$Day)
-
-
-# visualise the variable features being used
-celltype = 'Astrocytes'
-genes = relevant_chromatin_genes
-day1 = 'E18'
-day2 = 'P1'
-topic <- paste0(day1, '_', day2)
-
-print(paste0('Performing DGE for ', celltype, ', ', day1, ' vs ', day2))
-
-sobj_celltype <- subset(sobj1, subset = Cell_Type == celltype)
-Idents(sobj_celltype) <- 'Day'
-
-# Validate the Day clusters
-# Check if both day1 and day2 are present in the metadata column 'Day'
-all_days_present <- all(c(day1, day2) %in% sobj_celltype@meta.data$Day)
-# If not all days are present, print a message
-if (!all_days_present) {
-  missing_days <- setdiff(c(day1, day2), unique(sobj_celltype@meta.data$Day))
-  cat("The following days are missing in the Seurat object:", paste(missing_days, collapse = ", "), "\n")
-  return()
+# for days E18, P1, P4, and reference celltypes
+celltypes1 = c('Astrocytes', 'Microglia', 'Oligodendrocytes', 'Interneurons')
+celltypes2 = c('Migrating neurons', 'Interneurons', 'Microglia', 'Oligodendrocytes')
+for (day in days) {
+  for (celltype1 in celltypes1) {
+    for (celltype2 in celltypes2) {
+      tryCatch({
+        if (celltype1 != celltype2) {
+          perform_DGE_two_celltypes(sobj1, genes=relevant_chromatin_genes, day=day, celltype1=celltype1, celltype2=celltype2)
+        }
+      }, error = function(e) {
+        # Code to run in case of an error
+        cat("An error occurred:", e$message, "\n")
+      })
+    }
+  }
 }
-# Count the number of cells in each cluster
-cluster_counts <- table(sobj_celltype@meta.data$Day)
-# Check if there are any clusters with fewer than 3 cells
-if (cluster_counts[day1] < 3 || cluster_counts[day2] < 3) {
-  cat("At least one day has fewer than 3 cells:\n")
-  print(cluster_counts)
-  return()
-}
-
-sobj_celltype <- subset(sobj_celltype, subset = Day %in% c(day1, day2))
-
-filtered_chromatin_markers_per_day <- FindMarkers(
-  object = sobj_celltype,
-  features = genes,
-  ident.1 = day1,
-  ident.2 = day2,
-  min.pct = 0.25, # Gene must be detected in at least 25% of cells within a cluster
-  #min.diff.pct = 0.5,
-  logfc.threshold = 0.25, # Minimum log-fold change
-  test.use = 'wilcox', # Use Wilcoxon Rank Sum test
-  #p.adjust.method = 'bonferroni' # Bonferroni correction for multiple testing
-)
-
-print('Generating volcano plot')
-volcano_plot <- EnhancedVolcano(
-  filtered_chromatin_markers_per_day,
-  x='avg_log2FC', y='p_val',
-  lab='gene',
-  #pCutoff = 0.05
-) + ggtitle(paste0(celltype, ' DGE ', day1, ' vs ', day2)) +
-  theme(plot.title = element_text(hjust = 0.5))  # This centers the title
-#ggsave(figure_filename(sobj@project.name, topic, paste0(celltype, '_DGE_', topic, '_volcano')), plot = volcano_plot, width = 10, height = 8, dpi = 300)
-
-print('Generating significant marker list table')
-filtered_chromatin_markers_per_day_significant = filtered_chromatin_markers_per_day[filtered_chromatin_markers_per_day$p_val < 0.05,]
-#write.csv(filtered_chromatin_markers_per_day_significant, file = table_filename(sobj1@project.name, topic, paste0(celltype, '_', topic, '_DGE_markers')))
-
-print('Generating heatmap')
-DGE_per_day_heatmap = DoHeatmap(sobj_celltype, features = rownames(filtered_chromatin_markers_per_day_significant), group.by = 'Day', size = 3, angle = 90) +
-  ggtitle(paste0(celltype, ' DGE ', day1, ' vs ', day2)) +
-  theme(plot.title = element_text(hjust = 0.5))  # This centers the title
-dittoHeatmap(sobj_celltype, genes=rownames(filtered_chromatin_markers_per_day_significant), annot.by = 'Day', main = 'title',  )
-#ggsave(figure_filename(sobj@project.name, topic, paste0(celltype, '_DGE_', topic, '_heatmap')), plot = DGE_per_day_heatmap, width = 10, height = 8, dpi = 300)
 
 
 # Heatmaps for epigenetic modifiers
--- R/shared/plot_DGE_heatmap.R
+++ R/shared/plot_DGE_heatmap.R
@@ -1,42 +1,43 @@
 source('R/shared/filenames.R')
 
-perform_DGE_Two_Days <- function(sobj, celltype, genes, day1, day2) {
+perform_DGE_two_days <- function(sobj, genes, celltype, day1, day2, min.pct = 0.25, logfc.threshold = 0.25) {
   topic = 'DGE'
-  days = paste0(day1, '_', day2)
+  topic_suffix = paste0(day1, '_', day2)
+  days = c(day1, day2)
   
   print(paste0('Performing DGE for ', celltype, ', ', day1, ' vs ', day2))
   
-  sobj_celltype <- subset(sobj, subset = Cell_Type == celltype)
-  Idents(sobj_celltype) <- 'Day'
+  sobj_subset <- subset(sobj, subset = Cell_Type == celltype)
+  Idents(sobj_subset) <- 'Day'
   
   # Validate the Day clusters
   # Check if both day1 and day2 are present in the metadata column 'Day'
-  all_days_present <- all(c(day1, day2) %in% sobj_celltype@meta.data$Day)
+  all_clusters_present <- all(days %in% sobj_subset@meta.data$Day)
   # If not all days are present, print a message
-  if (!all_days_present) {
-    missing_days <- setdiff(c(day1, day2), unique(sobj_celltype@meta.data$Day))
-    cat("The following days are missing in the Seurat object:", paste(missing_days, collapse = ", "), "\n")
+  if (!all_clusters_present) {
+    missing <- setdiff(days, unique(sobj_subset@meta.data$Day))
+    cat("The following days are missing in the Seurat object:", paste(missing, collapse = ", "), "\n")
     return()
   }
   # Count the number of cells in each cluster
-  cluster_counts <- table(sobj_celltype@meta.data$Day)
+  cluster_counts <- table(sobj_subset@meta.data$Day)
   # Check if there are any clusters with fewer than 3 cells
   if (cluster_counts[day1] < 3 || cluster_counts[day2] < 3) {
-    cat("At least one day has fewer than 3 cells:\n")
+    cat("At least one day has fewer than 3 cells:")
     print(cluster_counts)
     return()
   }
   
-  sobj_celltype <- subset(sobj_celltype, subset = Day %in% c(day1, day2))
+  sobj_subset <- subset(sobj_subset, subset = Day %in% days)
   
   filtered_chromatin_markers_per_day <- FindMarkers(
-    object = sobj_celltype,
+    object = sobj_subset,
     features = genes,
     ident.1 = day1,
     ident.2 = day2,
-    min.pct = 0.25, # Gene must be detected in at least 25% of cells within a cluster
+    min.pct = min.pct, # Gene must be detected in at least 25% of cells within a cluster
     #min.diff.pct = 0.5,
-    logfc.threshold = 0.25, # Minimum log-fold change
+    logfc.threshold = logfc.threshold, # Minimum log-fold change
     test.use = 'wilcox', # Use Wilcoxon Rank Sum test
     #p.adjust.method = 'bonferroni' # Bonferroni correction for multiple testing
   )
@@ -49,90 +50,85 @@ perform_DGE_Two_Days <- function(sobj, celltype, genes, day1, day2) {
     #pCutoff = 0.05
   ) + ggtitle(paste0(celltype, ' DGE ', day1, ' vs ', day2)) +
     theme(plot.title = element_text(hjust = 0.5))  # This centers the title
-  ggsave(figure_filename(sobj@project.name, topic, paste0(celltype, '_DGE_', days, '_volcano')), plot = volcano_plot, width = 10, height = 8, dpi = 300)
+  ggsave(figure_filename(sobj@project.name, topic, paste0(celltype, '_DGE_', topic_suffix, '_volcano')), plot = volcano_plot, width = 10, height = 8, dpi = 300)
   
   print('Generating significant marker list table')
   filtered_chromatin_markers_per_day_significant = filtered_chromatin_markers_per_day[filtered_chromatin_markers_per_day$p_val < 0.05,]
-  write.csv(filtered_chromatin_markers_per_day_significant, file = table_filename(sobj@project.name, topic, paste0(celltype, '_', days, '_DGE_markers')))
+  write.csv(filtered_chromatin_markers_per_day_significant, file = table_filename(sobj@project.name, topic, paste0(celltype, '_', topic_suffix, '_DGE_markers')))
   
   print('Generating heatmap')
-  DGE_per_day_heatmap = dittoHeatmap(sobj_celltype, genes=rownames(filtered_chromatin_markers_per_day_significant), annot.by = 'Day', main = paste0(celltype, ' DGE ', day1, ' vs ', day2))
-  #DGE_per_day_heatmap = DoHeatmap(sobj_celltype, features = rownums(filtered_chromatin_markers_per_day_significant), group.by = 'Day', size = 3, angle = 90) +
-  #  ggtitle(paste0(celltype, ' DGE ', day1, ' vs ', day2)) +
-  #  theme(plot.title = element_text(hjust = 0.5))  # This centers the title
-  ggsave(figure_filename(sobj@project.name, topic, paste0(celltype, '_DGE_', days, '_heatmap')), plot = DGE_per_day_heatmap, width = 10, height = 8, dpi = 300)
+  DGE_per_day_heatmap = dittoHeatmap(sobj_subset, genes=rownames(filtered_chromatin_markers_per_day_significant), annot.by = 'Day', main = paste0(celltype, ' DGE ', day1, ' vs ', day2))
+  ggsave(figure_filename(sobj@project.name, topic, paste0(celltype, '_DGE_', topic_suffix, '_heatmap')), plot = DGE_per_day_heatmap, width = 10, height = 8, dpi = 300)
 }
 
-perform_DGE_two_celltypes <- function(sobj, genes, celltype1, celltype2, day) {
+perform_DGE_two_celltypes <- function(sobj, genes, celltype1, celltype2, day, min.pct = 0.25, logfc.threshold = 0.25) {
   topic = 'DGE'
-  celltypes = paste0(celltype1, '_', celltype2)
+  topic_suffix = paste0(celltype1, '_', celltype2)
+  celltypes = c(celltype1, celltype2)
   
   print(paste0('Performing DGE for ', day, ', ', celltype1, ' vs ', celltype2))
   
-  sobj_celltype <- subset(sobj, subset = Cell_Type == celltype)
-  Idents(sobj_celltype) <- 'Day'
+  sobj_subset <- subset(sobj, subset = Day == day)
+  Idents(sobj_subset) <- 'Cell_Type'
   
-  # Validate the Day clusters
-  # Check if both day1 and day2 are present in the metadata column 'Day'
-  all_days_present <- all(c(day1, day2) %in% sobj_celltype@meta.data$Day)
-  # If not all days are present, print a message
-  if (!all_days_present) {
-    missing_days <- setdiff(c(day1, day2), unique(sobj_celltype@meta.data$Day))
-    cat("The following days are missing in the Seurat object:", paste(missing_days, collapse = ", "), "\n")
+  # Validate the clusters
+  # Check if both celltypes are present in the metadata column 'Day'
+  all_clusters_present <- all(celltypes %in% sobj_subset@meta.data$Cell_Type)
+  # If not all clusters are present, print a message
+  if (!all_clusters_present) {
+    missing <- setdiff(celltypes, unique(sobj_subset@meta.data$Cell_Type))
+    cat("The following celltypes are missing in the Seurat object:", paste(missing, collapse = ", "), "\n")
     return()
   }
   # Count the number of cells in each cluster
-  cluster_counts <- table(sobj_celltype@meta.data$Day)
+  cluster_counts <- table(sobj_subset@meta.data$Cell_Type)
   # Check if there are any clusters with fewer than 3 cells
-  if (cluster_counts[day1] < 3 || cluster_counts[day2] < 3) {
-    cat("At least one day has fewer than 3 cells:\n")
+  if (cluster_counts[celltype1] < 3 || cluster_counts[celltype2] < 3) {
+    cat("At least one celltype has fewer than 3 cells:")
     print(cluster_counts)
     return()
   }
   
-  sobj_celltype <- subset(sobj_celltype, subset = Day %in% c(day1, day2))
+  sobj_subset <- subset(sobj_subset, subset = Cell_Type %in% celltypes)
   
-  filtered_chromatin_markers_per_day <- FindMarkers(
-    object = sobj_celltype,
+  markers_per_cluster <- FindMarkers(
+    object = sobj_subset,
     features = genes,
-    ident.1 = day1,
-    ident.2 = day2,
-    min.pct = 0.25, # Gene must be detected in at least 25% of cells within a cluster
+    ident.1 = celltype1,
+    ident.2 = celltype2,
+    min.pct = min.pct, # Gene must be detected in at least 25% of cells within a cluster
     #min.diff.pct = 0.5,
-    logfc.threshold = 0.25, # Minimum log-fold change
+    logfc.threshold = logfc.threshold, # Minimum log-fold change
     test.use = 'wilcox', # Use Wilcoxon Rank Sum test
     #p.adjust.method = 'bonferroni' # Bonferroni correction for multiple testing
   )
   
   print('Generating volcano plot')
   volcano_plot <- EnhancedVolcano(
-    filtered_chromatin_markers_per_day,
+    markers_per_cluster,
     x='avg_log2FC', y='p_val',
     lab='gene',
     #pCutoff = 0.05
-  ) + ggtitle(paste0(celltype, ' DGE ', day1, ' vs ', day2)) +
+  ) + ggtitle(paste0(day, ' DGE ', celltype1, ' vs ', celltype2)) +
     theme(plot.title = element_text(hjust = 0.5))  # This centers the title
-  ggsave(figure_filename(sobj@project.name, topic, paste0(celltype, '_DGE_', days, '_volcano')), plot = volcano_plot, width = 10, height = 8, dpi = 300)
+  ggsave(figure_filename(sobj@project.name, topic, paste0(day, '_DGE_', topic_suffix, '_volcano')), plot = volcano_plot, width = 10, height = 8, dpi = 300)
   
   print('Generating significant marker list table')
-  filtered_chromatin_markers_per_day_significant = filtered_chromatin_markers_per_day[filtered_chromatin_markers_per_day$p_val < 0.05,]
-  write.csv(filtered_chromatin_markers_per_day_significant, file = table_filename(sobj@project.name, topic, paste0(celltype, '_', days, '_DGE_markers')))
+  markers_per_cluster_significant = markers_per_cluster[markers_per_cluster$p_val < 0.05,]
+  write.csv(markers_per_cluster_significant, file = table_filename(sobj@project.name, topic, paste0(day, '_', topic_suffix, '_DGE_markers')))
   
   print('Generating heatmap')
-  DGE_per_day_heatmap = dittoHeatmap(sobj_celltype, genes=rownames(filtered_chromatin_markers_per_day_significant), annot.by = 'Day', main = paste0(celltype, ' DGE ', day1, ' vs ', day2))
-  #DGE_per_day_heatmap = DoHeatmap(sobj_celltype, features = rownums(filtered_chromatin_markers_per_day_significant), group.by = 'Day', size = 3, angle = 90) +
-  #  ggtitle(paste0(celltype, ' DGE ', day1, ' vs ', day2)) +
-  #  theme(plot.title = element_text(hjust = 0.5))  # This centers the title
-  ggsave(figure_filename(sobj@project.name, topic, paste0(celltype, '_DGE_', days, '_heatmap')), plot = DGE_per_day_heatmap, width = 10, height = 8, dpi = 300)
+  DGE_per_cluster_heatmap = dittoHeatmap(sobj_subset, genes=rownames(markers_per_cluster_significant), annot.by = 'Cell_Type', main = paste0(day, ' DGE ', celltype1, ' vs ', celltype2))
+  ggsave(figure_filename(sobj@project.name, topic, paste0(day, '_DGE_', topic_suffix, '_heatmap')), plot = DGE_per_cluster_heatmap, width = 10, height = 8, dpi = 300)
 }
 
 plot_DGE_heatmap <- function(sobj, celltype, relevant_chromatin_genes) {
-  #sobj_celltype <- subset(sobj, subset = Cell_Type_heuristic == 'astrocytes')
-  sobj_celltype <- subset(sobj, subset = Cell_Type == celltype)
-  Idents(sobj_celltype) <- 'Day'
+  #sobj_subset <- subset(sobj, subset = Cell_Type_heuristic == 'astrocytes')
+  sobj_subset <- subset(sobj, subset = Cell_Type == celltype)
+  Idents(sobj_subset) <- 'Day'
   
   filtered_chromatin_markers_all = FindAllMarkers(
-    object = sobj_celltype,
+    object = sobj_subset,
     features = relevant_chromatin_genes,
     #only.pos = TRUE, # Consider only positive markers
     min.pct = 0.25, # Gene must be detected in at least 25% of cells within a cluster
@@ -143,7 +139,7 @@ plot_DGE_heatmap <- function(sobj, celltype, relevant_chromatin_genes) {
   )
   
   # Heatmap for celltype per day
-  DGE_per_day_heatmap = DoHeatmap(sobj_celltype, features = filtered_chromatin_markers_all$gene, group.by = 'Day', size = 3, angle = 90) +
+  DGE_per_day_heatmap = DoHeatmap(sobj_subset, features = filtered_chromatin_markers_all$gene, group.by = 'Day', size = 3, angle = 90) +
     ggtitle(paste0(celltype, ' DGE per day')) +
     theme(plot.title = element_text(hjust = 0.5))  # This centers the title
 
